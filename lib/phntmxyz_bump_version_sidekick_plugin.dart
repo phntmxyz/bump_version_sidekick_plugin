d066ced35a74cbf767b1e8ed79fbda0e2cd58a5e
Pascal Welsch <pascal@welsch.dev>
2023-06-05 16:46:15 +0200

    Fix analysis

--- lib/phntmxyz_bump_version_sidekick_plugin.dart
+++ lib/phntmxyz_bump_version_sidekick_plugin.dart
@@ -1,3 +1,4 @@
+/// Provides a sidekick command that bumps the version in pubspec.yaml.
 library phntmxyz_bump_version_sidekick_plugin;
 
 export 'package:phntmxyz_bump_version_sidekick_plugin/src/bump_version.dart';
